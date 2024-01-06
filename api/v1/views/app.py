8fe5a17cc12940dfa12845dd0c6299e5e8e4bb2b
Abdurleone <abyyyusuf@gmail.com>
2024-01-06 16:05:10 +0300

    api

---
+++ api/v1/views/app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''Contains a Flask web application API.
+'''
+import os
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+from models import storage
+from api.v1.views import app_views
+
+
+app = Flask(__name__)
+'''The Flask web application instance.'''
+app_host = os.getenv('HBNB_API_HOST', '0.0.0.0.)
