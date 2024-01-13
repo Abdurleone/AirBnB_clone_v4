e7d906059e9cda38f4b66da3dfac69aa37ad1b14
Abdurleone <abyyyusuf@gmail.com>
2024-01-13 19:16:21 +0300

    v4

--- web_dynamic/2-hbnb.py
+++ web_dynamic/2-hbnb.py
@@ -23,7 +23,7 @@ def teardown_db(exception):
     storage.close()
 
 
-    @app.route('/2-hbnb/')
+@app.route('/2-hbnb/')
 def hbnb_filters(the_id=None):
     """
     handles request to custom template with states, cities & amentities
